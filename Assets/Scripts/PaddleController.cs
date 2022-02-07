4fa44a1e26c0d94736537cd111eb569735b43331
Dan C <djc5627@gmail.com>
2022-02-06 23:57:30 -0500

    Create PaddleController.cs. Connect PaddleController.cs to input actions. Connect Input System assembly to BMLScripts.

---
+++ Assets/Scripts/PaddleController.cs
@@ -0,0 +1,17 @@
+﻿using UnityEngine;
+using UnityEngine.InputSystem;
+
+namespace DefaultNamespace
+{
+    public class PaddleController : MonoBehaviour
+    {
+        [SerializeField] private Rigidbody2D paddleRb;
+        [SerializeField] private float moveSpeed = 5f;
+        
+        public void Move(InputAction.CallbackContext cntx)
+        {
+            Vector2 moveInput = cntx.ReadValue<Vector2>();
+            paddleRb.velocity = moveInput * moveSpeed;
+        }
+    }
+}
