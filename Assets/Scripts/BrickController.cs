3083f3f555d3ad962bb1099ea0024ae8574084d0
Dan C <djc5627@gmail.com>
2022-02-07 00:38:11 -0500

    Create BrickController.cs.

---
+++ Assets/Scripts/BrickController.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using UnityEngine;
+
+namespace DefaultNamespace
+{
+    public class BrickController : MonoBehaviour
+    {
+        [SerializeField] private string ballTag = "Ball";
+        private void OnCollisionEnter2D(Collision2D other)
+        {
+            Debug.Log("Received");
+            
+            if (other.gameObject.tag.Equals(ballTag))
+                Destroy(this);
+        }
+    }
+}
