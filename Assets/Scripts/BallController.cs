65ebec971f443673f7ce753b61b4e9b2e8f7ea55
merc37 <gabeajh@gmail.com>
2022-02-19 01:02:32 -0500

    Paddle hit location influencing angle is now weighted between reflections

--- Assets/Scripts/BallController.cs
+++ Assets/Scripts/BallController.cs
@@ -1,17 +1,18 @@
-﻿using System;
+﻿using MyAssets.ScriptableObjects.Variables;
 using UnityEngine;
+using Sirenix.OdinInspector;
 
 namespace BML.Scripts
 {
-    public class BallController : MonoBehaviour
+    public class BallController : SerializedMonoBehaviour
     {
         [SerializeField] private Rigidbody2D ballRb;
         [SerializeField] private Vector2 initialDirection;
-        [SerializeField] private float initialSpeed = 5f;
+        [SerializeField] private FloatReference ballSpeed;
 
         private void Start()
         {
-            ballRb.velocity = initialDirection * initialSpeed;
+            ballRb.velocity = initialDirection.normalized * ballSpeed.Value;
         }
     }
 }
--- Assets/Scripts/PaddleController.cs
+++ Assets/Scripts/PaddleController.cs
@@ -1,16 +1,20 @@
-﻿using System;
+﻿using MyAssets.ScriptableObjects.Variables;
 using UnityEngine;
 using UnityEngine.InputSystem;
+using Sirenix.OdinInspector;
 
 namespace BML.Scripts
 {
-    public class PaddleController : MonoBehaviour
+    public class PaddleController : SerializedMonoBehaviour
     {
         [SerializeField] private Rigidbody2D paddleRb;
         [SerializeField] private Collider2D paddleCollider;
         [SerializeField] private float moveSpeed = 5f;
-        [SerializeField] private AnimationCurve angularInfluence;
+        [SerializeField] private AnimationCurve angularInfluenceWeight;
+        [SerializeField] private float maxAngularInfluence = 60f;
         [SerializeField] private string ballTag = "Ball";
+        [SerializeField] private bool locationInfluencesAngle = true;
+        [SerializeField] private FloatReference ballSpeed;
 
         public void Move(InputAction.CallbackContext cntx)
         {
@@ -20,20 +24,26 @@ namespace BML.Scripts
 
         private void OnCollisionEnter2D(Collision2D other)
         {
-            if (other.collider.CompareTag(ballTag))
+            if(other.collider.CompareTag(ballTag))
             {
-                var contactPoint = other.contacts[0].point;
-                var differenceFromCenter = contactPoint.x - paddleRb.position.x;
-                var sign = Mathf.Sign(differenceFromCenter);
-                var distanceFromCenter = Mathf.Abs(differenceFromCenter);
-            
-                var paddleWidth = paddleCollider.bounds.extents.x;
-                var distFac = (2 * distanceFromCenter) / (paddleWidth);
-                var angle = angularInfluence.Evaluate(distFac);
-                var rotationInfluence = Quaternion.AngleAxis(angle * sign, Vector3.back);
-            
-                var speed = other.rigidbody.velocity.magnitude;
-                other.rigidbody.velocity = rotationInfluence * paddleRb.transform.up * speed;
+                if(locationInfluencesAngle)
+                {
+                    var contactPoint = other.contacts[0].point;
+                    var differenceFromCenter = contactPoint.x - paddleRb.position.x;
+                    var sign = Mathf.Sign(differenceFromCenter);
+                    var distanceFromCenter = Mathf.Abs(differenceFromCenter);
+                    var paddleWidth = paddleCollider.bounds.extents.x;
+
+                    var distFac = (2 * distanceFromCenter) / (paddleWidth);
+                    var weight = angularInfluenceWeight.Evaluate(distFac);
+
+                    var maxRotationInfluence = Quaternion.AngleAxis(maxAngularInfluence * sign, Vector3.back);
+
+                    var reflectVelocity = other.rigidbody.velocity.normalized * ballSpeed.Value;
+                    var maxReflectVelocity = maxRotationInfluence * Vector3.up * ballSpeed.Value;
+
+                    other.rigidbody.velocity = Vector2.Lerp(reflectVelocity, maxReflectVelocity, weight);
+                }
             }
         }
     }
