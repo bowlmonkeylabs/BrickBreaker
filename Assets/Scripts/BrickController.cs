5e5d5f8382148ed250250a64c9017954197a306b
Max Lepkowski <mlepkows@stevens.edu>
2022-02-27 18:36:31 -0500

    Add feedback for brick breaking.

--- Assets/Scripts/BrickController.cs
+++ Assets/Scripts/BrickController.cs
@@ -1,4 +1,5 @@
-﻿using MyAssets.ScriptableObjects.Events;
+﻿using MoreMountains.Feedbacks;
+using MyAssets.ScriptableObjects.Events;
 using UnityEngine;
 
 namespace BML.Scripts
@@ -8,16 +9,22 @@ namespace BML.Scripts
         [SerializeField] private string ballTag = "Ball";
         [SerializeField] private GameEvent brickBroken;
         [SerializeField] private GameObject leanTweenOwner;
+        [SerializeField] private Collider2D brickCollider;
+        [SerializeField] private MMFeedbacks onBreakFeedbacks;
 
         private void OnCollisionEnter2D(Collision2D other)
         {
             if(other.gameObject.tag.Equals(ballTag))
             {
-                Destroy(gameObject);
-                LeanTween.value(leanTweenOwner, 0, 1, 0.01f).setOnComplete(() => {
-                    brickBroken.Raise();
-                });
+                BreakBrick();
             }
+        }
+
+        private void BreakBrick()
+        {
+            brickCollider.enabled = false;
+            onBreakFeedbacks.PlayFeedbacks();
+            brickBroken.Raise();
         }
     }
 }
