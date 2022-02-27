147155834e4f6ca02abfbfa26a8200bfaaece243
Dan C <djc5627@gmail.com>
2022-02-27 13:09:40 -0500

    Add aim targeter. Add catch trigger. Paddle catches Ball.

--- Assets/Scripts/PaddleController.cs
+++ Assets/Scripts/PaddleController.cs
@@ -1,4 +1,5 @@
-﻿using MyAssets.ScriptableObjects.Variables;
+﻿using System;
+using MyAssets.ScriptableObjects.Variables;
 using UnityEngine;
 using UnityEngine.InputSystem;
 using Sirenix.OdinInspector;
@@ -8,18 +9,59 @@ namespace BML.Scripts
     public class PaddleController : SerializedMonoBehaviour
     {
         [SerializeField] private Rigidbody2D paddleRb;
+        [SerializeField] private Rigidbody2D ballRb;
+        [SerializeField] private Transform aimTargeter;
         [SerializeField] private Collider2D paddleCollider;
+        [SerializeField] private Transform ballCatchPivot;
         [SerializeField] private float moveSpeed = 5f;
+        [SerializeField] private float aimTurnSpeed = 5f;
         [SerializeField] private AnimationCurve angularInfluenceWeight;
         [SerializeField] private float maxAngularInfluence = 60f;
         [SerializeField] private string ballTag = "Ball";
         [SerializeField] private bool locationInfluencesAngle = true;
         [SerializeField] private FloatReference ballSpeed;
 
-        public void Move(InputAction.CallbackContext cntx)
+        private Vector2 moveInput;
+        private bool isBallInCatchTrigger;
+        private bool isBallCaught;
+
+        private void Update()
         {
-            Vector2 moveInput = cntx.ReadValue<Vector2>();
-            paddleRb.velocity = moveInput * moveSpeed;
+            if (isBallCaught)
+                aimTargeter.Rotate(Vector3.back, moveInput.x * aimTurnSpeed * Time.deltaTime);
+            else
+                paddleRb.velocity = moveInput * moveSpeed;
+            
+        }
+
+        private void FixedUpdate()
+        {
+            if (isBallCaught)
+                ballRb.MovePosition(ballCatchPivot.position);
+        }
+
+        public void ReceiveMove(InputAction.CallbackContext cntx)
+        {
+            moveInput = cntx.ReadValue<Vector2>();
+        }
+
+        public void ReceiveCatch(InputAction.CallbackContext cntx)
+        {
+            if (!isBallInCatchTrigger || !cntx.performed) return;
+            
+            if (isBallCaught)
+            {
+                //Release
+                ballRb.velocity = aimTargeter.up * ballSpeed.Value;
+                isBallCaught = false;
+                aimTargeter.gameObject.SetActive(false);
+                return;
+            }
+
+            isBallCaught = true;
+            paddleRb.velocity = Vector2.zero;
+            aimTargeter.gameObject.SetActive(true);
+            aimTargeter.rotation = Quaternion.identity;
         }
 
         private void OnCollisionEnter2D(Collision2D other)
@@ -45,6 +87,18 @@ namespace BML.Scripts
                     other.rigidbody.velocity = Vector2.Lerp(reflectVelocity, maxReflectVelocity, weight);
                 }
             }
+        }
+
+        private void OnTriggerEnter2D(Collider2D other)
+        {
+            if (other.CompareTag(ballTag))
+                isBallInCatchTrigger = true;
+        }
+
+        private void OnTriggerExit2D(Collider2D other)
+        {
+            if (other.CompareTag(ballTag))
+                isBallInCatchTrigger = false;
         }
     }
 }
