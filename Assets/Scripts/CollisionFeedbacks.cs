198c5ce14246e2e54111a622892100ae3eb883c8
Max Lepkowski <mlepkows@stevens.edu>
2022-03-03 23:35:51 -0500

    Feature/juice (#1)
    * Duplicate main scene for this branch.
    
    * Add tags/layers for bricks and paddle.
    
    * Add configurable script for feedback on collision.
    
    * Add placeholder sounds for ball collision.
    
    * Add sound for paddle hit. Change ball bounce sound and add pitch variation.
    
    * Add trail to ball.
    
    * Add feedback for brick breaking.
    
    * Adjust brick break feedback.
    
    * Update ball trail.
    
    * Add meta files.
    
    * Fix win condition by checking brick state instead of existence.
    
    * Add win/lose sounds.
    
    * Merge branch changes into main scene.

---
+++ Assets/Scripts/CollisionFeedbacks.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using MoreMountains.Feedbacks;
+using Sirenix.OdinInspector;
+using UnityEngine;
+
+namespace BML.Scripts
+{
+    [Serializable]
+    public struct CollisionType
+    {
+        public LayerMask layerMask;
+        public string tag;
+        [LabelText("Include/Exclude Tag")] public bool includeExcludeTag;
+        public MMFeedbacks feedbacks;
+    }
+    
+    public class CollisionFeedbacks : MonoBehaviour
+    {
+        [SerializeField] private List<CollisionType> collisionTypes;
+
+        private void OnCollisionEnter2D(Collision2D col)
+        {
+            var colLayer = col.gameObject.layer;
+            var colTag = col.gameObject.tag;
+            collisionTypes.ForEach(collisionType =>
+            {
+                var matchesLayer = collisionType.layerMask == (collisionType.layerMask | (1 << colLayer));
+                var matchesTag = string.IsNullOrEmpty(collisionType.tag) 
+                                 || (collisionType.includeExcludeTag == (collisionType.tag == colTag));
+                if (matchesLayer && matchesTag)
+                {
+                    collisionType.feedbacks.PlayFeedbacks();
+                }
+            });
+        }
+    }
+}
